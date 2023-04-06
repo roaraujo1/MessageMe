ffe1ad4cb9c3cd06e93117712f64622875ccce6b
Rosario <Rosario@Rosario-MacBook-Pro.local>
2023-04-06 13:48:41 -0700

    added project structure and created homescreen

---
+++ components/Homescreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text, Button } from "react-native";
+
+const Homescreen = () => {
+  return (
+    <View>
+      <Button
+        title= "Log in"/>
+    </View>
+  );
+};
+export default Homescreen;
