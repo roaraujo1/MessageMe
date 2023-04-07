919898224e4c7aa23540ddd2d4c1f135ef17c98c
Rosario <Rosario@Rosario-MacBook-Pro.local>
2023-04-07 12:44:48 -0700

    added basic footer

--- components/Homescreen.js
+++ components/Homescreen.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import FlatButton from "../ui/FlatButton";
+import Footer from "../ui/Footer";
 
 const Homescreen = (props) => {
   return(
     <View>
       <FlatButton title={"Login"} onPress={props.handleSubmit} />
       <FlatButton title={"Create Account"} onPress={props.handleSubmit} />
+      <Footer/>
     </View>
 
   );
---
+++ ui/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text,View,StyleSheet } from "react-native";
+
+const Footer = ()=>{
+    return(
+        <View>
+            <Text>MessageMe</Text>
+        </View>
+    )
+
+};
+export default Footer;
